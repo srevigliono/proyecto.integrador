e020e3feedc05d37fe0e8f388d66a7d80ae6dfa3
Bautista Juarez <bjuarez@udesa.edu.ar>
2023-11-10 13:47:59 -0300

    seguimos con el punto de el formulario

--- js/search-results.js
+++ js/search-results.js
@@ -2,29 +2,29 @@ let formulario = document.querySelector(".buscador")
 let moviesapi = "https://api.themoviedb.org/3/movie/now_playing?api_key=1173214cf5e2ac8f2c0ac1c242d0ec8a"
 
 let querystringmovies = location.search
-console.log(location.search)
 
 let querystringmoviesobj = new URLSearchParams(querystringmovies)
-console.log(querystringmoviesobj.get("resultadosbusqueda"))
+let querystringformulario = querystringmoviesobj.get("formulario")
+console.log(querystringformulario)
 
 fetch(moviesapi)
-.then(function(response) {
-  return response.json()
-})
-.then(function(data) {
-    console.log(data);
-    console.log(data.results)
-    let contenidomovies = data.results
-    formulario.addEventListener("submit" , function() {
-
-
-
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let contenido = data.results;
+    if (querystringformulario == "Movies") {
+      for (let i = 0; i < contenido.length; i++) {
+        let titulopelicula = contenido[i].title;
+        console.log(titulopelicula);
+      }
     }
-    )
-
-
-
-})
-.catch(function(error) {
-  console.log("Error: " + error);
-})
+      for (let i = 0; i < contenido.length; i++) {
+        let titulopelicula = contenido[i].title;
+        console.log(titulopelicula);
+     
+      }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  })
