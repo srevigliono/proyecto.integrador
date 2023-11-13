f034626cffd5566b24f1197af8d8152697e8d4cf
Bautista Juarez <bjuarez@udesa.edu.ar>
2023-11-13 16:10:34 -0300

    hacieendo el punto dos del trabajo, nose porque no me deja linkearlo y lo que hago en java no aparece en detail

---
+++ js/detail-movie.js
@@ -0,0 +1,56 @@
+let formulario = document.querySelector(".buscador");
+
+let querystring = location.search;
+console.log(querystring);
+
+let querystringobj = new URLSearchParams(querystring);
+
+let informacionformulario = querystringobj.get("formulario");
+
+console.log(informacionformulario);
+
+let url = `https://api.themoviedb.org/3/search/movie?api_key=3e70f944e54851d50cccbf55e9b26736&query=${informacionformulario}`;
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    let movies = data.results;
+    console.log(movies);
+
+    let contenido = "";
+    let searchResults = document.querySelector("#sectiondetalle");
+
+    for (let i = 0; i < movies.length; i++) {
+      contenido +=
+        `<article>
+          <img src="https://image.tmdb.org/t/p/w500/${movies[i].poster_path}" alt="Movie Poster">
+        </article>
+        
+        <article>
+          <h2>${movies[i].title}</h2>
+          
+          <div class="trailer">
+            <div class="info-trailer">
+              <h5><span style="text-decoration: underline; margin-right: 8px;">FECHA DE ESTRENO:</span> ${movies[i].release_date}</h5>
+              <h5><span style="text-decoration: underline; margin-right: 8px;">DURACIÓN:</span>${movies[i].vote_average}</h5>
+              <h5><span style="text-decoration: underline; margin-right: 8px;">IDIOMA:</span>${movies[i].original_language}</h5> 
+            </div>
+          </div>
+          
+          <div class="parrafo-trailer">
+            <p>${movies[i].overview}</p>
+          </div>
+          
+          <h4 class="boton-agregar-favoritos">AGREGAR A FAVORITOS</h4>
+        </article>`;
+    }
+
+    searchResults.innerHTML = contenido;
+  })
+  .catch(function (error) {
+    console.log("Error: " + error);
+  });
+
--- js/search-results.js
+++ js/search-results.js
@@ -12,53 +12,36 @@ console.log(informacionformulario);
 let url = `https://api.themoviedb.org/3/search/movie?api_key=3e70f944e54851d50cccbf55e9b26736&query=${informacionformulario}`;
 
 fetch(url)
-
-.then(function(response) {
-    return response.json()
-    })
-
-.then(function(data) {
-    console.log(data)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
     let movies = data.results;
-    console.log(movies)
-
+    console.log(movies);
 
     let contenido = "";
-    let i = 0;
     let searchResults = document.querySelector("#searchresults");
+
+    let texto = document.querySelector("#titulobusqueda");
+    let resultadosbusqueda = texto.innerText = `Resultados de búsqueda para: ${informacionformulario}`;
+
     for (let i = 0; i < movies.length; i++) {
-    
-        contenido += `<a href="./detail-movie.html${movies[i].id}">
-            <article>
-                <img src="https://image.tmdb.org/t/p/w500/${movies[i].poster_path}" alt="Movie Poster">
-                <h4>${movies[i].title}</h4>
-                <p>${movies[i].release_date} | ${movies[i].vote_average}</p>
-            </article>
-        </a>`
+      contenido += `<a href="./detail-movie.html?id=${movies[i].id}">
+          <article>
+              <img src="https://image.tmdb.org/t/p/w500/${movies[i].poster_path}" alt="Movie Poster">
+              <h4>${movies[i].title}</h4>
+              <p>${movies[i].release_date} | ${movies[i].vote_average}</p>
+          </article>
+        </a>`;
     }
 
-     for (let i = 0; i < movies.length; i++) {
-    
-        contenido += `<a href="./detail-movie.html${movies[i].id}">
-            <article>
-                <img src="https://image.tmdb.org/t/p/w500/${movies[i].poster_path}" alt="Movie Poster">
-                <h4>${movies[i].title}</h4>
-                <p>${movies[i].release_date} | ${movies[i].vote_average}</p>
-            </article>
-        </a>`
-    }
-    if (movies.length==0) {
-        contenido += `<h3>¡No hay resultados para tu busqueda!</h3>`
+    if (movies.length == 0) {
+      contenido += `<h3>¡No hay resultados para tu búsqueda!</h3>`;
     }
 
     searchResults.innerHTML = contenido;
-
-   })
-
-.catch(function(error) {
-    console.log("Error: " + error);
-    });
-
-
-
-
+  })
+  .catch(function(error) {
+    console.log("Error en la solicitud API: " + error);
+  });
