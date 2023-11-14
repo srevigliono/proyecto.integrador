bb3456df0951dd27576ed16281feb0595ac8443a
Santiago Revigliono <srevigliono@udesa.edu.ar>
2023-11-14 17:26:21 -0300

    Consigna 4 casi terminada, solo falta corregir detalles. Avanzamos con la consigna 6.

---
+++ js/detail-genres.js
@@ -0,0 +1,44 @@
+
+let formulario = document.querySelector(".buscador");
+
+let querystring = location.search;
+console.log(querystring);
+
+let querystringobj = new URLSearchParams(querystring);
+
+let informacionformulario = querystringobj.get("formulario");
+
+console.log(informacionformulario);
+
+let APIKey = "3e70f944e54851d50cccbf55e9b26736";
+let urlpeliculasGeneros = `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKey}&query=${informacionformulario}`;
+
+fetch(urlpeliculasGeneros)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let generos = data.results;
+    console.log(generos);
+
+    let contenido = "";
+    let searchResults = document.querySelector("#peliculasGeneros");
+
+    for (let i = 0; i < peliculasGeneros.length; i++) {
+      contenido +=
+      `<a href="./detail-movie.html=${generos[i].id}">
+      <article>
+           <img src="./Imagenes/pelicula2012.jpeg=${generos[i].poster_path}" alt="2012">
+           <h4>${generos[i].title}</h4>
+           <p>${generos[i].release_date} | ${generos[i].vote_average}</p>
+      </article>
+  </a>`;
+    }
+
+    searchResults.innerHTML = contenido;
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
+
--- js/detail-movie.js
+++ js/detail-movie.js
@@ -11,6 +11,7 @@ console.log(informacionformulario);
 
 let url = `https://api.themoviedb.org/3/search/movie?api_key=3e70f944e54851d50cccbf55e9b26736&query=${informacionformulario}`;
 
+
 fetch(url)
   .then(function(response) {
     return response.json();
@@ -23,27 +24,28 @@ fetch(url)
     let contenido = "";
     let searchResults = document.querySelector("#sectiondetalle");
 
-    for (let i = 0; i < detalle.length; i++) {
+ 
+   if (detalle.length>0){
       contenido +=
         `<article>
-          <img src="https://image.tmdb.org/t/p/w500/${detalle[i].poster_path}" alt="Movie Poster">
+          <img src="https://image.tmdb.org/t/p/w500/${detalle[0].poster_path}" alt="Movie Poster">
         </article>
   
         <article>
-          <h2>${detalle[i].title}</h2>
+          <h2>${detalle[0].title}</h2>
   
           <div class="info-trailer">
-            <h5><span style="text-decoration: underline; margin-right: 8px;">FECHA DE ESTRENO:</span>${detalle[i].release_date}</h5>
-            <h5><span style="text-decoration: underline; margin-right: 8px;">DURACIÓN:</span>${detalle[i].runtime} mins</h5>
-            <h5><span style="text-decoration: underline; margin-right: 8px;">GENERO:</span>${detalle[i].genres}</h5>
-            <h5><span style="text-decoration: underline; margin-right: 8px;">SIPNOSIS:</span>${detalle[i].overview}</h5>
-            <h5><span style="text-decoration: underline; margin-right: 8px;">CALIFICACIÓN:</span>${detalle[i].vote_average}</h5>
+            <h5><span style="text-decoration: underline; margin-right: 8px;">FECHA DE ESTRENO:</span>${detalle[0].release_date}</h5>
+            <h5><span style="text-decoration: underline; margin-right: 8px;">DURACIÓN:</span>${detalle[0].runtime} mins</h5>
+            <h5><span style="text-decoration: underline; margin-right: 8px;">GENERO:</span>${detalle[0].genres}</h5>
+            <h5><span style="text-decoration: underline; margin-right: 8px;">SIPNOSIS:</span>${detalle[0].overview}</h5>
+            <h5><span style="text-decoration: underline; margin-right: 8px;">CALIFICACIÓN:</span>${detalle[0].vote_average}</h5>
           </div>
   
           <h4 class="boton-agregar-favoritos">AGREGAR A FAVORITOS</h4>
         </article>`;
-    }
-
+   }
+  
     searchResults.innerHTML = contenido;
   })
   .catch(function(error) {
